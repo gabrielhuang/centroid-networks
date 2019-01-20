919edac82c9dad739da99fd54cc4f66676a36443
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-01-20 00:31:26 +0000

    add cuda

--- run_unsupervised.sh
+++ run_unsupervised.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #PYTHONPATH=. python2 scripts/train/few_shot/run_train.py --log.exp_dir results.supervised.sinkhorn.centroid --model.model_name clusternet_conv --mode supervised --supervisedsinkhorn 1 --centroid-loss 1 --data.cuda
-PYTHONPATH=. python2 scripts/train/few_shot/run_train.py --log.exp_dir results.supervised.sinkhorn.centroid.20way --model.model_name clusternet_conv --mode supervised --supervisedsinkhorn 1 --centroid-loss 1 --data.cuda --data.way 20 --data.test_way 20
+PYTHONPATH=. python2 scripts/train/few_shot/run_train.py --log.exp_dir results.supervised.sinkhorn.centroid.20way --model.model_name clusternet_conv --supervisedsinkhorn 1 --centroid-loss 1 --data.cuda --data.way 20 --data.test_way 20 --data.cuda
