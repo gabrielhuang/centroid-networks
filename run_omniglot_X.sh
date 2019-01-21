7cf7d9fd0d485030bfbb48d52657051812a7882d
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-01-21 02:16:30 +0000

    kill all with ctrl c

--- run_omniglot_X.sh
+++ run_omniglot_X.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #PYTHONPATH=. python2 scripts/train/few_shot/run_train.py --log.exp_dir results.supervised.sinkhorn.centroid --model.model_name clusternet_conv --mode supervised --supervisedsinkhorn 1 --centroid-loss 1 --data.cuda
 
+trap "kill 0" SIGINT
+
 WAYS=$1
 COMMON="python2 scripts/train/few_shot/run_train.py --data.cuda --model.model_name clusternet_conv --data.way $WAYS --data.test_way $WAYS"
 
-PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/sinkhorn.centroid0.1 --train-loss sinkhorn --centroid-loss 0.1 & \
-PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/sinkhorn.centroid0.01 --train-loss sinkhorn --centroid-loss 0.01& \
-PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/sinkhorn.centroid0.001 --train-loss sinkhorn --centroid-loss 0.001& \
-PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/softmax.centroid0.1 --train-loss softmax --centroid-loss 0.1 & \
-PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/softmax.centroid0.01 --train-loss softmax --centroid-loss 0.01& \
-PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/softmax.centroid0.001 --train-loss softmax --centroid-loss 0.001&& fg
+(PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/sinkhorn.centroid0.1 --train-loss sinkhorn --centroid-loss 0.1) & \
+(PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/sinkhorn.centroid0.01 --train-loss sinkhorn --centroid-loss 0.01)& \
+(PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/sinkhorn.centroid0.001 --train-loss sinkhorn --centroid-loss 0.001)& \
+(PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/softmax.centroid0.1 --train-loss softmax --centroid-loss 0.1 )& \
+(PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/softmax.centroid0.01 --train-loss softmax --centroid-loss 0.01)& \
+(PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/softmax.centroid0.001 --train-loss softmax --centroid-loss 0.001)
+
+wait
+echo "All runs complete."
