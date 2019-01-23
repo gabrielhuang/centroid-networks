5981f576d8883ec50cd7a3e40c6afed33e1c3c32
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-01-23 21:50:10 +0000

    evaluation script

---
+++ eval_miniimagenet_best.sh
@@ -0,0 +1,4 @@
+
+COMMON="python2 scripts/train/few_shot/run_train.py --data.cuda --data.dataset miniimagenet --data.root $HOME/data/miniimagenet --model.model_name clusternet_conv --iterations 30000"
+
+PYTHONPATH=. $COMMON --log.exp_dir results/miniimagenet/sinkhorn.temperature1.centroid1 --train-loss evalonly --data.way 5
