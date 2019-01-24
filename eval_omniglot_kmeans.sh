f780e32c8b01e5aaa6ce862c61c9071e791eeed6
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-01-24 00:11:57 +0000

    eval kmeans

---
+++ eval_omniglot_kmeans.sh
@@ -0,0 +1,6 @@
+
+COMMON="python2 scripts/train/few_shot/run_train.py --data.cuda --model.model_name clusternet_conv --iterations 1000"
+
+
+WAYS=$1
+PYTHONPATH=. $COMMON --log.exp_dir results/omniglot${WAYS}/kmeans --train-loss softmax --clustering kmeansplusplus --train-loss evalonly --data.way $WAYS --data.test_way $WAYS --clustering kmeansplusplus --regularization 1 # avoid running many times
