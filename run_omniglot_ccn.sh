270a8aa98b5a094a0704da647b0fa5550ed782a2
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-01-28 16:57:07 +0000

    change centroid loss for ccn to 0.1

--- run_omniglot_ccn.sh
+++ run_omniglot_ccn.sh
@@ -2,4 +2,4 @@
 
 COMMON="python2 scripts/train/few_shot/run_train.py --data.dataset omniglot_ccn --data.cuda --model.model_name clusternet_conv"
 
-PYTHONPATH=. $COMMON --log.exp_dir results/omniglot_ccn --train-loss sinkhorn --centroid-loss 1 --regularization 1
+PYTHONPATH=. $COMMON --log.exp_dir results/omniglot_ccn0.1 --train-loss sinkhorn --centroid-loss 0.1 --regularization 1
