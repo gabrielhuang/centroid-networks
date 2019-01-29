9e46ff25b1bff7a416501d498ff70b9715857557
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-01-29 00:27:05 +0000

    early stopping

--- shell/omniglot_ccn/run_omniglot_ccn_archproto.sh
+++ shell/omniglot_ccn/run_omniglot_ccn_archproto.sh
@@ -3,4 +3,4 @@
 TRAINLOSS=$1
 COMMON="python2 ../../scripts/train/few_shot/run_train.py --data.dataset omniglot_ccn --data.cuda --model.model_name clusternet_conv"
 
-PYTHONPATH=../.. $COMMON --log.exp_dir results/arch_protonet --train-loss $TRAINLOSS --centroid-loss 0.1 --regularization 1 --iterations 30000 
+PYTHONPATH=../.. $COMMON --log.exp_dir results/arch_protonet --train-loss $TRAINLOSS --centroid-loss 0.1 --regularization 1 --iterations 10000 
