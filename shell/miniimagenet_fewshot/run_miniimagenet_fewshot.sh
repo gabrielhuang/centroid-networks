e77864745ce25432bf873b1259cd38097bc92565
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-02-15 15:40:58 +0000

    fix eval not use checkpoint

--- shell/miniimagenet_fewshot/run_miniimagenet_fewshot.sh
+++ shell/miniimagenet_fewshot/run_miniimagenet_fewshot.sh
@@ -7,4 +7,10 @@ PATH_TO_PRETRAINED="$HOME/code/cyvius96/save/proto-5/epoch-last.pth"
 
 COMMON="python2 ../../scripts/train/few_shot/run_train.py --data.cuda --data.dataset miniimagenet --data.root $HOME/data/miniimagenet --model.model_name clusternet_conv --iterations 30000"
 
-PYTHONPATH=../.. $COMMON --log.exp_dir results/miniimagenet5 --train-loss $TRAINLOSS --centroid-loss 1 --data.way 5 --checkpoint-state $PATH_TO_PRETRAINED
+if [[ "$TRAINLOSS" == "evalonly" ]]
+then
+	PYTHONPATH=../.. $COMMON --log.exp_dir results/miniimagenet5 --train-loss $TRAINLOSS --centroid-loss 1 --data.way 5
+else
+	PYTHONPATH=../.. $COMMON --log.exp_dir results/miniimagenet5 --train-loss $TRAINLOSS --centroid-loss 1 --data.way 5 --checkpoint-state $PATH_TO_PRETRAINED
+fi
+
