aba9ada1fc76af2952dd40e72824a174256423ac
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-03-13 06:54:01 +0000

    ablation miniimagenet

--- shell/miniimagenet_fewshot/ablation_miniimagenet.sh
+++ shell/miniimagenet_fewshot/ablation_miniimagenet.sh
@@ -11,12 +11,12 @@ echo "Will output results in directory $DIR"
 # Replace this accordingly
 PATH_TO_PRETRAINED="$HOME/code/cyvius96/save/proto-5/epoch-last.pth"
 
-COMMON="python2 ../../../scripts/train/few_shot/run_train.py --data.cuda --data.dataset miniimagenet --data.root $HOME/data/miniimagenet --model.model_name clusternet_conv --iterations 30000 --data.way 5"
+COMMON="python2 ../../scripts/train/few_shot/run_train.py --data.cuda --data.dataset miniimagenet --data.root $HOME/data/miniimagenet --model.model_name clusternet_conv --iterations 30000 --data.way 5"
 
 if [[ "$MODE" == "eval" ]]
 then
-	PYTHONPATH=../../.. $COMMON --log.exp_dir $DIR --train-loss evalonly --centroid-loss $CENTERLOSS --clustering $CLUSTERING
+	PYTHONPATH=../.. $COMMON --log.exp_dir $DIR --train-loss evalonly --centroid-loss $CENTERLOSS --clustering $CLUSTERING
 else
-	PYTHONPATH=../../.. $COMMON --log.exp_dir $DIR --train-loss $TRAINLOSS --checkpoint-state $PATH_TO_PRETRAINED --centroid-loss $CENTERLOSS --clustering $CLUSTERING
+	PYTHONPATH=../.. $COMMON --log.exp_dir $DIR --train-loss $TRAINLOSS --checkpoint-state $PATH_TO_PRETRAINED --centroid-loss $CENTERLOSS --clustering $CLUSTERING
 fi
 
