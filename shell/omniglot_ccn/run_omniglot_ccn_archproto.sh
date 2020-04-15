e363420b627c4a558eccde6b72e179b632d183c5
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-15 21:10:24 +0000

    add clustering algorithm option to ccn archproto

--- shell/omniglot_ccn/run_omniglot_ccn_archproto.sh
+++ shell/omniglot_ccn/run_omniglot_ccn_archproto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TRAINLOSS=$1
+CLUSTERING=${2:-wasserstein}
 COMMON="python2 ../../scripts/train/few_shot/run_train.py --data.dataset omniglot_ccn --data.cuda --model.model_name clusternet_conv"
 
-PYTHONPATH=../.. $COMMON --log.exp_dir results/arch_protonet --train-loss $TRAINLOSS --centroid-loss 0.1 --regularization 1 --iterations 10000 
+PYTHONPATH=../.. $COMMON --log.exp_dir results/arch_protonet_$CLUSTERING --train-loss $TRAINLOSS --centroid-loss 0.1 --regularization 1 --iterations 10000 --clustering $CLUSTERING
